b0fc1fa910151d9ab3f9c4d7ed7d0c34200c2a21
dugu007 <dasdurga923@gmail.com>
2022-01-18 01:02:43 +0530

    Adding Dynamic Styles to Chart

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -16,8 +16,6 @@ const ExpenseItem = (props) => {
           <h2>{title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        {/* <button onClick={() => console.log("clicked!!")}>Change title</button> */}
-        <button onClick={clickHandler}>Change title</button>
       </Card>
     </li>
   );
